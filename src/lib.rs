253dfb1805454676cce8e0cd4ef246ccef12a9b4
Woshiluo Luo <woshiluo.luo@outlook.com>
2020-06-02 09:39:50 +0800

    Add Sandbox Status

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ use std::process::Command;
 use std::process::Stdio;
 use std::error::Error;
 
+use time::prelude::*;
+
 #[derive(Debug)]
 pub struct SandboxConfig {
     /// Rootfs 目录
@@ -12,7 +14,7 @@ pub struct SandboxConfig {
     /// 时间限制（以 ms 为单位）
     pub time_limit: u32,
     /// 内存限制（以 Byte 为单位）
-    pub memory_limit: u32,
+    pub memory_limit: u64,
 
     /// Work Directory  
     /// 执行时只有这个目录是可写的  
@@ -28,15 +30,22 @@ pub struct SandboxConfig {
     pub stderr: Stdio
 }
 
-// enum sandbox_status_kind {
-//     time_limit_exceeded,
-//     memory_limit_exceeded,
-//     runtime_error,
-//     success
-// }
-// 
-// struct sandbox_status {
-// }
+#[derive(Debug)]
+pub enum SandboxStatusKind {
+    TimeLimitExceeded,
+    MemoryLimitExceeded,
+    RuntimeError,
+    Success
+// tle > mle > re > seccess
+}
+
+#[derive(Debug)]
+pub struct SandboxStatus {
+    status: SandboxStatusKind,
+    used_time: i128,
+    max_memory: u64,
+    return_code: i32
+}
 
 /// 根据 SandboxConfig 来执行命令
 ///
@@ -46,7 +55,7 @@ pub struct SandboxConfig {
 /// sanbox_run( SandboxConfig{ 
 ///     rootfs_directory,
 ///     time_limit: 1000,
-///     memory_limit: 512,
+///     memory_limit: 512 * 1024 * 1024,
 /// 
 ///     work_directory: work_directory,
 /// 
@@ -59,7 +68,7 @@ pub struct SandboxConfig {
 ///         panic!( "{}", err );
 /// });
 /// ```
-pub fn sanbox_run( config: SandboxConfig ) -> Result< (), Box<dyn Error> > {
+pub fn sanbox_run( config: SandboxConfig ) -> Result< SandboxStatus, Box<dyn Error> > {
     use cgroups_fs::CgroupsCommandExt;
     let cgroup_name = "wsl-sandbox";
 
@@ -90,27 +99,56 @@ pub fn sanbox_run( config: SandboxConfig ) -> Result< (), Box<dyn Error> > {
         .spawn()?.wait()?;
     log::info!( "Done!" );
 
-    log::info!( "New cgourps {} create", cgroup_name );
+    // Create new cgroup
+    log::info!( "New cgroup {} create", cgroup_name );
     let cur_cgroup = cgroups_fs::CgroupName::new( cgroup_name );
     let cur_cgroup = cgroups_fs::AutomanagedCgroup::init(&cur_cgroup, "memory")?;
-    log::info!( "Memory {}", config.memory_limit * 2 );
+    log::info!( "Memory Limit {}", config.memory_limit * 2 );
     cur_cgroup.set_value( "memory.limit_in_bytes", config.memory_limit * 2 )?;
 
     // Run command
     let command = format!( "cd /tmp/{};{}", cgroup_name, config.command ); 
     log::debug!( "Chroot {} to run '{}'", target_rootfs_directory, command );
-    let q = std::process::Command::new( "timeout" )
+
+    let time_start = time::Instant::now();
+    let return_code = std::process::Command::new( "timeout" )
         .args( &[ "10", 
                "chroot", &target_rootfs_directory, 
                "bash", "-c", &command ] )
         .cgroups(&[&cur_cgroup])
-//        .output()?;
         .stdin( config.stdin )
         .stdout( config.stdout )
         .stderr( config.stderr )
-        .spawn()?.wait()?;
-    log::debug!( "{:?}", q );
+        .status()?.code();
+    let time_end = time::Instant::now();
 
+    let mut status = SandboxStatusKind::Success;
+
+    log::debug!( "Return code: {:?}", return_code );
+    log::debug!( "Start at {:?}, End at {:?}, Use at {:?}", time_start, time_end, time_end - time_start );
+
+    // Get return code
+    let return_code = match return_code {
+        Some(code) => code,
+        None => { return Err( String::from( "Process terminated by signal" ).into() ); }
+    };
+    if return_code != 0 {
+        status = SandboxStatusKind::RuntimeError;
+    }
+
+    // Calc Memory 
+    let max_memory = cur_cgroup.get_value::<u64>("memory.max_usage_in_bytes").unwrap();
+    if max_memory > config.memory_limit {
+        status = SandboxStatusKind::MemoryLimitExceeded;
+    }
+
+    // Calc time
+    let used_time = time_end - time_start;
+    log::debug!( "{:?}", used_time );
+    if used_time > config.time_limit.milliseconds() {
+        status = SandboxStatusKind::TimeLimitExceeded;
+    }
+    let used_time = used_time.whole_milliseconds();
 
     log::info!( "Remove Sandbox" );
     // Umount rootfs
@@ -124,7 +162,12 @@ pub fn sanbox_run( config: SandboxConfig ) -> Result< (), Box<dyn Error> > {
         .spawn()?.wait()?;
     log::info!( "Done!" );
 
-    Ok( () )
+    Ok( SandboxStatus{
+        status,
+        max_memory,
+        used_time,
+        return_code
+    } )
 }
 
 #[cfg(test)]
@@ -135,7 +178,7 @@ mod test {
         sanbox_run( SandboxConfig{ 
             rootfs_directory: "/work/package/debs/debian-rootfs/".to_string(),
             time_limit: 1000,
-            memory_limit: 512,
+            memory_limit: 512 * 1024 * 1024,
 
             work_directory: "/home/woshiluo/tmp".to_string(),
 
--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,17 @@ use std::process::Stdio;
 fn main() {
     pretty_env_logger::init();
     log::info!( "Start Program" );
-    sanbox_run( SandboxConfig{ 
+    let status = sanbox_run( SandboxConfig{ 
         rootfs_directory: "/work/package/debs/debian-rootfs/".to_string(),
         time_limit: 1000,
         memory_limit: 1024 * 1024 * 512,
 
         work_directory: "/home/woshiluo/tmp".to_string(),
 
-        command: "./a.out".to_string(),
+//        command: "echo \"Hello, World\"".to_string(),
+//        command: String::from( "sleep 5" ),
+        command: String::from( "./a.out" ),
+//        command: String::from( "g++ temp.cpp" ),
 
         stdin: Stdio::null(),
         stdout: Stdio::inherit(),
@@ -23,4 +26,6 @@ fn main() {
         log::error!( "Failed to run sandbox: {}", err );
         panic!( "{}", err );
     });
+
+    log::debug!( "{:?}", status );
 }
