bd81b57a08c88dfcfa41c39be4e52422961f7ca3
zsq001 <zsq1750819212@gmail.com>
2020-06-07 00:44:04 +0800

    添加测试cpp

---
+++ test/change.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main()
+{
+		system("echo 1 >> /home/zsq001/ttttt");
+		return 0;
+}
+
---
+++ test/tle.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+		for(long long i=1;i<=20000147483647;i++)
+		cout<<1;
+		return 0;
+}
